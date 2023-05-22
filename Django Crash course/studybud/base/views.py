55518fa34182c6d71f8b3872e336686de74c1ab8
dot-ruth <ruthgetaneh5@gmail.com>
2023-05-22 16:32:29 +0300

    studybud-Django crash course

---
+++ Django Crash course/studybud/base/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm
+
+
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
