515689eccb42b4f93c97cb67569e3e6b4df828d5
dot-ruth <ruthgetaneh5@gmail.com>
2023-05-23 17:06:57 +0300

    added more functionalities

--- Django Crash course/studybud/base/urls.py
+++ Django Crash course/studybud/base/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('room/<str:pk>',views.room,name="room"),
-    path('create-room',views.createRoom,name="create-room")
+    path('login', views.loginpage, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+    path('register', views.registerUser, name="register"),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('user-profile/<str:pk>/', views.userprofile, name="user-profile"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 ]
--- Django Crash course/studybud/base/views.py
+++ Django Crash course/studybud/base/views.py
@@ -1,22 +1,170 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic, Message
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
+def loginpage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "user doesn't exsit")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "username or password is incorrect")
+
+    # through the context we communicate to the templates
+    context = {'page': page}
+    return render(request, 'base/login_registration.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured while registration')
+
+    context = {'form': form}
+    return render(request, 'base/login_registration.html', context)
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
-    return render(request,'base/home.html',context)
+    '''when rendering the home if the 'q' is empty it should show all the topic 
+       that's why the if statement is needed
 
-def room(request,pk):
+       q is the topic name that is sent as a request in the home html
+    '''
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    # the topic__name__icontains
+    # filter(topic=q) raised an error the filter function expects an id but got the topic instead
+    # filter(topic__name=q) the double underscore is b/c of the inheritance (to avoid naming conflict in Python subclasses) -- this works fine but the home page
+    # is empty when q is set to none
+    # filter(topic__name__contains) works fine but is case sensitive
+    # filter(topic__name__icontains) works fine but is case insensitive
+    # Q (imported from django.db.models) is to incapsulate many parameters in the filter functionality
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) | Q(description__icontains=q))
+    room_count = rooms.count
+    room_message = Message.objects.filter(Q(room__name__icontains=q))
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_message': room_message}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
-    return render(request,'base/room.html',context)
+    room_message = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_message': room_message,
+               'participants': participants}
+    return render(request, 'base/room.html', context)
+
+
+def userprofile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_message = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+               'room_message': room_message, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
-    context = {'form':form}
-    return render(request,'base/room_form.html',context)
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('Your are not allowed here')
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('Your are not allowed here')
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {'obj': message}
+    return render(request, 'base/delete.html', context)
