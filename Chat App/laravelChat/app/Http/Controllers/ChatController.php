d72fee91f36b5d3e03c145dd8dee139666e64cdc
dot-ruth <ruthgetaneh5@gmail.com>
2023-09-14 10:45:44 +0300

    Added Chat App and Laravel Starter Kit

---
+++ Chat App/laravelChat/app/Http/Controllers/ChatController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\Message;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    //
+    public function message(Request $request)
+    {
+        event(new Message($request->username, $request->message));
+        return response()->json([
+            'username' => $request->username,
+            'message' => $request->message
+        ]);
+    }
+}
